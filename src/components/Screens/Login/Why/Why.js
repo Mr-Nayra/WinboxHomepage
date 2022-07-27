2a06ab9cf100d8d840fc674237e9d6721de454e0
Mr.Nayra <aryanedu5786@gmail.com.com>
2022-07-27 12:34:04 +0530

    Mobile responsove 2

--- src/components/Screens/Login/Why/Why.js
+++ src/components/Screens/Login/Why/Why.js
@@ -26,30 +26,36 @@ const Why = (props) => {
         <Cont className={classes.block}>
           <div className={classes.flexcol}>
             <Heading326 className={classes.head}>Email warm-up</Heading326>
-            <Row className={classes.row} content="Automated interactions" />
-            <Row className={classes.row} content="Auto-Remove from Spam" />
-            <Row
-              className={classes.row}
-              content="Human realistic emails (GPT-3)"
-            />
-            <Row className={classes.row} content="Spam score monitoring" />
+            <div className={classes.tickRowCont}>
+              <Row className={classes.row} content="Automated interactions" />
+              <Row className={classes.row} content="Auto-Remove from Spam" />
+              <Row
+                className={classes.row}
+                content="Human realistic emails (GPT-3)"
+              />
+              <Row className={classes.row} content="Spam score monitoring" />
+            </div>
           </div>
           <div className={classes.flexcol}>
-            <Heading326 className={classes.head}>Inbox integrations</Heading326>
-            <Row
-              className={classes.row}
-              content="Gmail - Gsuite inbox (OAuth)"
-            />
-            <Row className={classes.row} content="Outlook - Microsoft 365" />
-            <Row className={classes.row} content="Yahoo Inbox" />
-            <Row className={classes.row} content="Any other Inbox (SMTP)" />
+            <Heading326 className={classes.head}>Inbox integrations</Heading326>{" "}
+            <div className={classes.tickRowCont}>
+              <Row
+                className={classes.row}
+                content="Gmail - Gsuite inbox (OAuth)"
+              />
+              <Row className={classes.row} content="Outlook - Microsoft 365" />
+              <Row className={classes.row} content="Yahoo Inbox" />
+              <Row className={classes.row} content="Any other Inbox (SMTP)" />
+            </div>
           </div>
           <div className={classes.flexcol}>
-            <Heading326 className={classes.head}>Account & services</Heading326>
-            <Row className={classes.row} content="Team members" />
-            <Row className={classes.row} content="Live chat support" />
-            <Row className={classes.row} content="In-app Onboarding" />
-            <Row className={classes.row} content="Blog resources" />
+            <Heading326 className={classes.head}>Account & services</Heading326>{" "}
+            <div className={classes.tickRowCont}>
+              <Row className={classes.row} content="Team members" />
+              <Row className={classes.row} content="Live chat support" />
+              <Row className={classes.row} content="In-app Onboarding" />
+              <Row className={classes.row} content="Blog resources" />
+            </div>
           </div>
         </Cont>
       </Container>
